a29c81eafee8e78c69e6d3fb3afa405a84adf706
Christopher Lopes <christopher.lopes@gmail.com>
2013-10-20 12:00:24 -0700

    Use string functions instead of regex's in the bob exercise

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -3,20 +3,18 @@
 (defn question? [s]
   (= \? (last s)))
 
-(defn said-words? [s]
-  (re-find #"[a-zA-Z]" s))
-
 (defn shouting? [s]
-  (and (said-words? s) (not (re-find #"[a-z]" s))))
+  (and
+    (= (clojure.string/upper-case s) s)
+    (not (= (clojure.string/lower-case s) s))))
 
 (defn silence? [s]
-  (re-matches #"\s*" s))
+  (clojure.string/blank? s))
 
 (defn response-for [statement]
   (cond
     (silence? statement) "Fine. Be that way!"
     (shouting? statement) "Woah, chill out!"
     (question? statement) "Sure."
-    :else "Whatever."
-    ))
+    :else "Whatever."))
 
