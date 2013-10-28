f977036de0c47f36a13885f6ace8427aa67d1a1f
Christopher Lopes <christopher.lopes@gmail.com>
2013-10-28 09:18:48 -0700

    Word count solution using frequencies

--- clojure/word-count/word-count.clj
+++ clojure/word-count/word-count.clj
@@ -1,13 +1,9 @@
 (ns phrase)
 
-(defn- map-word-to-word-list [word-list word]
-  (assoc word-list word (inc (word-list word 0))))
-
 (defn- generate-words [s]
-  ; A word can consist of unicode letters or digits
+  "Returns a lazy sequence of words, consisting of unicode
+  letters or digits, in string."
   (re-seq #"\p{L}+|\d+" (.toLowerCase s)))
 
 (defn word-count [s]
-  (reduce map-word-to-word-list
-          {}
-          (generate-words s)))
+  (frequencies (generate-words s)))
