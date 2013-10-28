56b701c700ee9574797bf550203c2b27d17f773f
Christopher Lopes <christopher.lopes@gmail.com>
2013-10-27 21:00:06 -0700

    While researching a better solution the following example solution was found: http://en.wikibooks.org/wiki/Clojure_Programming/By_Example. The key points in this solution is using the reduce function to create a single map from each map for each word. The second point is using the default of 0 if a key does not already exist in the map.

--- clojure/word-count/word-count.clj
+++ clojure/word-count/word-count.clj
@@ -1,26 +1,13 @@
 (ns phrase)
 
-(defn- create-key-name [word]
-  (apply str (keep
-               #(if (or (Character/isDigit %) (Character/isLetter %))
-                  (Character/toLowerCase %))
-               word)))
+(defn- map-word-to-word-list [word-list word]
+  (assoc word-list word (inc (word-list word 0))))
 
-(defn- increment-word-count [key-name word-list]
-  (let [word (keyword key-name)] 
-    (if (word word-list)
-      (assoc word-list word (inc (word word-list)))
-      (assoc word-list word 1))))
-
-(defn- create-keyword-map [word-list words]
-  (if (empty? words)
-    word-list
-    (let [key-name (create-key-name (first words))] 
-      (if (empty? key-name)
-        (recur word-list (rest words))
-        (recur (increment-word-count key-name word-list) (rest words)))))) 
+(defn- generate-words [s]
+  ; A word can consist of unicode letters or digits
+  (re-seq #"\p{L}+|\d+" (.toLowerCase s)))
 
 (defn word-count [s]
-  (into {} 
-        (for [[k v] (create-keyword-map {} (clojure.string/split s #"\s+")) ] 
-          [(name k) v])))
+  (reduce map-word-to-word-list
+          {}
+          (generate-words s)))
