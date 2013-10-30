ccc4eb6e4f875f2509593838396f6234d90b890a
Christopher Lopes <christopher.lopes@gmail.com>
2013-10-29 20:18:24 -0700

    Remove docstring from word count

--- clojure/word-count/word-count.clj
+++ clojure/word-count/word-count.clj
@@ -1,12 +1,9 @@
 (ns phrase)
 
 (defn- format-word [s]
-  "Returns string in lower case form with any characters
-  which are not unicode letters or digits removed."
   (clojure.string/replace (.toLowerCase s) #"[^\p{L}\d]" ""))
 
 (defn- generate-words [s]
-  "Returns a lazy sequence of words in string."
   (remove empty? (map
                    format-word
                    (re-seq #"[^\s]+" s))))
